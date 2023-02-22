6ea4f0a5fb1d9e4a56fb00f6869f7f06fca597d8
silviadwiy <117156657+silviadwiy@users.noreply.github.com>
2023-02-22 08:35:55 +0700

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -6,12 +6,18 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     print("Starting Evaluation.....")
     print("Submission related metadata:")
 
-    df1 = pd.read_csv(test_annotation_file, index_col='text_id')
-    df2 = pd.read_csv(user_submission_file, index_col='text_id')
+    df1 = pd.read_csv(test_annotation_file)
+    df2 = pd.read_csv(user_submission_file)
+
+    df1 = df1.drop('text_id', axis=1)
+    df2 = df2.drop('text_id', axis=1)
 
     # Extract the target variables from each data frame
-    y_true = df1[['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions']]
-    y_pred = df2[['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions']]
+    # y_true = df1[['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions']]
+    # y_pred = df2[['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions']]
+
+    y_true = df1
+    y_pred = df2
 
     # Calculate MCRMSE
     mcrmse_score = np.sqrt(mean_squared_error(y_true, y_pred, multioutput='raw_values')).mean()
